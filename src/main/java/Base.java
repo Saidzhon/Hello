6d8592c62967d5a484df39bb0416394d82fedc75
Saidzhon <a.s.habibullaevich@gmail.com>
2018-03-11 14:08:59 +0300

    +комменты

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,13 +1,19 @@
 import java.io.PrintStream;
 import java.util.Scanner;
+
 public class Base {
 
     public static void main(String[] args) {
+        /*ввод первого числа*/
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the First number");
         int a = scanner.nextInt();
+
+        /*ввод второго числа*/
         System.out.println("Enter the Second number");
         int b = scanner.nextInt();
+
+        /*отображение результата*/
         System.out.println("Sum is:");
         System.out.print(a + b);
         scanner.close();
