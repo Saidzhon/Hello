08838ad75d7156310f8e8d9cd68b0c79593115c4
Saidzhon <a.s.habibullaevich@gmail.com>
2018-03-14 11:10:30 +0300

    добавил возможность умножения, деления и вычитания.

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Base {
 
     /** Реазилация кальтулятора*/
-    
+
     public static void main(String[] args) {
         /*ввод первого числа*/
         Scanner scanner = new Scanner(System.in);
@@ -15,9 +15,22 @@ public class Base {
         System.out.println("Enter the Second number");
         double b = scanner.nextDouble();
 
-        /*отображение результата*/
+        /*отображение результата сложения*/
         System.out.println("Sum is:");
         System.out.printf("%.4f",a + b);
+
+        /*отображение результата вычитания*/
+        System.out.println("\nSub is:");
+        System.out.printf("%.4f",a - b);
+
+        /*отображение результата умножения*/
+        System.out.println("\nMult is:");
+        System.out.printf("%.4f",a * b);
+
+        /*отображение результата деления*/
+        System.out.println("\nDiv is:");
+        System.out.printf("%.4f",a / b);
+
         scanner.close();
 
     }
