9e35aea88e2a886af95a8541f7bded588df6dfab
Saidzhon <a.s.habibullaevich@gmail.com>
2018-03-17 19:54:53 +0300

    + возможность выбора операции

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.spi.ior.IdentifiableFactory;
+
 import java.io.PrintStream;
 import java.util.Scanner;
 
@@ -6,32 +8,56 @@ public class Base {
     /** Реазилация кальтулятора*/
 
     public static void main(String[] args) {
-        /*ввод первого числа*/
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the First number");
-        double a = scanner.nextDouble();
+   
+   
+   
 
-        /*ввод второго числа*/
-        System.out.println("Enter the Second number");
-        double b = scanner.nextDouble();
 
-        /*отображение результата сложения*/
-        System.out.println("Sum is:");
-        System.out.printf("%.4f",a + b);
 
-        /*отображение результата вычитания*/
-        System.out.println("\nSub is:");
-        System.out.printf("%.4f",a - b);
+    /*ввод первого числа*/
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the First number");
+    double firstNumber = scanner.nextDouble();
 
-        /*отображение результата умножения*/
-        System.out.println("\nMult is:");
-        System.out.printf("%.4f",a * b);
+    /*ввод второго числа*/
+    System.out.println("Enter the Second number");
+    double secondNumber = scanner.nextDouble();
+    int operation;
 
-        /*отображение результата деления*/
-        System.out.println("\nDiv is:");
-        System.out.printf("%.4f",a / b);
+    Scanner scanner1 = new Scanner(System.in);
+                    System.out.println("Введите цифру, соответствующую желаемой операции");
+                    System.out.println("1 - Sum");    /*сложение*/
+                    System.out.println("2 - Sub");    /*вычитание*/
+                    System.out.println("3 - Mult");   /*умножение*/
+                    System.out.println("4 - Div");    /*деление*/
+                    operation = scanner1.nextInt();
+      scanner1.close();
+                    switch (operation) {
 
-        scanner.close();
+                        case 1:
+                            /*отображение результата сложения*/
+                            System.out.println("Sum is:");
+                            System.out.printf("%.4f", firstNumber + secondNumber);
+                            break;
 
+                        case 2:
+                            /*отображение результата вычитания*/
+                            System.out.println("\nSub is:");
+                            System.out.printf("%.4f", firstNumber - secondNumber);
+                            break;
+                        case 3:
+                            /*отображение результата умножения*/
+                            System.out.println("\nMult is:");
+                            System.out.printf("%.4f", firstNumber * secondNumber);
+                            break;
+
+                        case 4:
+                            /*отображение результата деления*/
+                            System.out.println("\nDiv is:");
+                            System.out.printf("%.4f", firstNumber / secondNumber);
+                            break;
+
+
+                    }
     }
 }
