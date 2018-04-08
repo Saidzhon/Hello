152cc7d4fbfc238185aa38505d827291980c49dc
Saidzhon <a.s.habibullaevich@gmail.com>
2018-04-08 15:36:42 +0300

    Выполнено задание 5

--- src/main/java/CalcOOP/CalcOperations/CalcOperations.java
+++
@@ -1,5 +0,0 @@
-package CalcOOP.CalcOperations;
-
-public abstract class CalcOperations {
-    public abstract float calculus(float x, float y);
-}
--- src/main/java/CalcOOP/CalcOperations/CalcReports.java
+++
@@ -1,7 +0,0 @@
-package CalcOOP.CalcOperations;
-
-public interface CalcReports {
-
-    float calculus(float x, float y);
-    String getSing();
-}
